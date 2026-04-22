6b550666201065a6b8e8e082a5910e724d223f05
Kelvin Justine <justineikechi6@gmail.com>
2026-04-22 16:25:50 +0100

    Setting up the project folders

---
+++ hirely-backend/src/test/java/com/project/hirely_backend/HirelyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hirely_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirelyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
